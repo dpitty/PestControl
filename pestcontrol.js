2dcdc259223b27fdd2fba2112a3a4e77a17270a8
David Pitt <dpitt00@gmail.com>
2014-08-11 20:33:23 -0700

    Pretty deece

--- pestcontrol.js
+++ pestcontrol.js
@@ -2,7 +2,7 @@
 /*	pestcontrol.js
  *  Pest Control core functionality
  *	===============================
- *		CHANGES: <hr> is now outside of note div
+ *	
  */
 
 
@@ -10,35 +10,31 @@
 
 var PestControl = (function() {
 	/* Keycodes */
-	var RETURN_KEY = 13; //keypresss v
+	var RETURN_KEY = 13;
 	var E_KEY = 69;
 	var A_KEY = 65;
 	var I_KEY = 73;
-	var UPARROW_KEY = 38;  //keydown
-	var DOWNARROW_KEY = 40; //keydown
+	var UPARROW_KEY = 38;
+	var DOWNARROW_KEY = 40;
 	var ESCAPE_KEY = 27;
 	var BACKSPACE_KEY = 8;
 
+	/* Initializers and Helpers */
 	var notesBlock = document.getElementById('notesBlock');
 	var instructionsBox = document.getElementById('instructionsBox');
-
-	function noteByIndex(i) {
-		return notesBlock.children[i];
-	}
-
-
-	function highlight(index) {
-		noteByIndex(index).children[0].className = "highlight";
-	}
-
-	function unhilight(index) {
-		noteByIndex(index).children[0].className = "";
-	}
-
-	//length = notes.children.length
 	var index = 0;
 	var editing = false;
-	highlight(index);
+	var clicked = false;
+	var instructionsUp = false;
+	
+	(function initTop() {
+		var toInsert = document.createElement("div");
+		toInsert.innerHTML = "<div id='insertTop'><p>&gt;&gt;New top note</p></div><hr/>"
+			//Hate to hard code but this makes most sense for now.
+		notesBlock.insertBefore(toInsert, noteByIndex(0));
+		console.log("did it.")
+		highlight(index);
+	})();
 
 	function tag(text) {
 		return "<div><p>"+text+"</p></div><hr/>" //to be placed inside "DIV" node
@@ -48,9 +44,22 @@ var PestControl = (function() {
 		return "<textarea id='textField'>'"+text+"</textarea>";
 	}
 
+
+	function noteByIndex(i) {
+		return notesBlock.children[i];
+	}
+
+	function highlight(index) {
+		noteByIndex(index).children[0].className = "highlight";
+	}
+
+	function unhilight(index) {
+		noteByIndex(index).children[0].className = "";
+	}
+
+	/* Core functions */
 	function newNote(text) {
-		//parentElement.insertBefore(newElement, referenceElement);
-		noteByIndex(index).firstChild.className = "";		
+		unhilight(index);	
 		var ta = document.createElement("textarea");
 		ta.id = "textarea";
 		notesBlock.insertBefore(ta, noteByIndex(index + 1));
@@ -68,14 +77,13 @@ var PestControl = (function() {
 		newNote(text);
 	}
 
-	//Whatever is in the textarea will become a note
-	function finishNote() {
+	function finishNote() { //Whatever is in the textarea will become a note
 		var toInsert = document.createElement("div");
 		var ta = document.getElementById("textarea");
 		toInsert.innerHTML = tag(ta.value);
 		ta.parentNode.removeChild(ta);
-		if (ta.value == "") {
-			noteByIndex(index).firstChild.className = "highlight";
+		if (ta.value == "") { //Empty textarea? Same effect as deleteNote()
+			highlight(index);
 			editing = false;
 		}
 		else {
@@ -94,10 +102,10 @@ var PestControl = (function() {
 	}
 
 	function scrollUp() {
-		noteByIndex(index).firstChild.className = "";
+		unhilight(index);
 		index--;
 		if (index < 0) { index = notesBlock.children.length - 1}
-		noteByIndex(index).firstChild.className = "highlight";
+		highlight(index);
 	}
 
 	function scrollDown() {
@@ -108,63 +116,83 @@ var PestControl = (function() {
 	}
 
 	function toggleInstructions() {
-		if (instructionsBox.className) {
+		if (!instructionsUp) {
 			instructionsBox.className = "";
+			instructionsUp = true;
 		}
 		else {
 			instructionsBox.className = "hidden";
+			instructionsUp = false;
 		}
 	}
 
+
+	/* Event Listeners */
 	document.body.onclick = function(e) {
-		//alert("YOUR MOUSE IS USELESS! To see instructions, press 'I' (letter i) >:-O");
-		if (!editing) {
+		if (!editing && !clicked) {
 			toggleInstructions();
+			clicked = true;
 		}
 	}
 
 	document.body.onkeydown = function(e) {
 
-
-		if (!editing) {
+		if (!editing && !instructionsUp) {
 			switch(e.keyCode) {
+				
 				case BACKSPACE_KEY:
 					e.preventDefault();
 					deleteNote();
 					break;
+				
 				case RETURN_KEY:
 					e.preventDefault();
 					newNote("");
 					break;
+				
 				case E_KEY:
 					e.preventDefault();
 					editNote();
 					break;
-				case I_KEY:
-					e.preventDefault();
-					toggleInstructions();
-					break;
+				
 				case UPARROW_KEY:
 					e.preventDefault();
 					scrollUp();
 					break;
+				
 				case DOWNARROW_KEY:
 					e.preventDefault();
 					scrollDown();
 					break;
-				}	
+					
+				case I_KEY:
+					e.preventDefault();
+					toggleInstructions();
+					break;
+				}
 			}
 
-		else {
+		else if (editing) { //Edit mode on...
 			switch(e.keyCode) {
+				
 				case RETURN_KEY:
 					e.preventDefault();
 					finishNote();
 					break;
 				}
 			}
+
+		else if (instructionsUp) {
+			switch(e.keyCode) {
+				case I_KEY:
+					e.preventDefault();
+					toggleInstructions();
+					break;
+				}
+			}
 		}
 
+	/* For debugging purposes... */
 	return {
 		notesBlock: notesBlock.children,
 		index: function() { return index; }
